a005f22a6a82bc3b36fc32bbfcabc6df72ab515d
s13g <ayflix0@gmail.com>
2022-11-09 00:55:27 +0100

    adding more apps

--- projects/forms.py
+++ projects/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 
 from projects.models import Project
@@ -11,3 +12,13 @@ class ProjectForm(ModelForm):
         model = Project
         fields = "__all__"
         exclude = ["vote_ratio", "vote_total"]
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(), # making the checkboxes better
+        }
+
+    # adding styles to the form by adding the css class to be modified
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
