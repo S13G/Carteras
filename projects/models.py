39f9210c437a2a842f21046370890f0cde1b13e8
s13g <ayflix0@gmail.com>
2022-11-09 02:18:14 +0100

    worked on the user, profiles, and projects

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+
 from users.models import Profile
 
 
--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from users.models import Profile
+from users.models import Profile, Skill
 
 # Register your models here.
 
-admin.site.register(Profile)
+admin.site.register([Profile, Skill])
