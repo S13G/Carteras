44a2316a544f3a1efa88f8ffd8d4af0f43af38f2
s13g <ayflix0@gmail.com>
2022-12-11 00:59:14 +0100

    password

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('', include('users.urls')),
+    # password reset urls and views plus templates
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="reset_password.html"),
          name='reset_password'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="reset_password_sent.html"),
