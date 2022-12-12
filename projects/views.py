ec4b74304fb95213385cf89908380b648bfec12c
s13g <ayflix0@gmail.com>
2022-12-12 11:31:48 +0100

    remove tag fixes

--- projects/views.py
+++ projects/views.py
@@ -83,7 +83,7 @@ def updateProject(request, pk):
             tag, created = Tag.objects.get_or_create(name=tag)
             project.tags.add(tag)
         return redirect('projects')
-    context = {"form": form}
+    context = {"form": form, "project": project}
     return render(request, 'projects/project-form.html', context)
 
 
