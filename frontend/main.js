86c397cd1635b2cc8b3e523f4e71e26a87ef12fd
s13g <ayflix0@gmail.com>
2022-12-17 13:59:46 +0100

    sending data from frontend

--- frontend/main.js
+++ frontend/main.js
@@ -9,8 +9,9 @@ let getProjects = () => {
 
 let buildProjects = (projects) => {
     let projectsWrapper = document.getElementById('projects--wrapper')
+    projectsWrapper.innerHTML = ''
 
-    for (let i=0; projects.length > i; i++ ) {
+    for (let i = 0; projects.length > i; i++) {
         let project = projects[i]
 
         let projectCard = `
@@ -20,8 +21,8 @@ let buildProjects = (projects) => {
                 <div>
                     <div class="card--header">
                         <h3>${project.title}</h3>
-                        <strong class="vote--option">&#43;</strong>
-                        <strong class="vote--option">&#8722;</strong>
+                        <strong class="vote--option" data-vote="up" data-project="${project.id}">&#43;</strong>
+                        <strong class="vote--option" data-vote="down" data-project="${project.id}">&#8722;</strong>
                     </div>
                     <i>${project.vote_ratio}% Positive feedback</i>
                     <p>${project.description.substring(0, 150)}</p>
@@ -31,6 +32,32 @@ let buildProjects = (projects) => {
 
         projectsWrapper.innerHTML += projectCard
     }
+
+    addVoteEvents()
+
 }
 
+// Add listeners
+let addVoteEvents = () => {
+    let voteBtns = document.getElementsByClassName('vote--option')
+
+    for (let i = 0; i < voteBtns.length; i++) {
+        voteBtns[i].addEventListener('click', (e) => {
+            let token = localStorage.getItem("token")
+            let vote = e.target.dataset.vote
+            let project = e.target.dataset.project
+
+            fetch(`http://127.0.0.1:8000/api/project/${project}/vote/`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
+                },
+                body:JSON.stringify({'value': vote})
+            }).then(response => response.json()).then(data => {
+                getProjects()
+            })
+        })
+    }
+}
 getProjects()
