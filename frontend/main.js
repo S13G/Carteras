d594c2104abd581c62d26336ac79c6eae59f8054
s13g <ayflix0@gmail.com>
2022-12-17 14:43:17 +0100

    api section and frontend done

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,21 @@
+let loginBtn = document.getElementById('login-btn')
+let logoutBtn = document.getElementById('logout-btn')
+
+let token = localStorage.getItem("token")
+
+if (token) {
+    loginBtn.remove()
+} else {
+    logoutBtn.remove()
+}
+
+logoutBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    localStorage.removeItem('token')
+    window.location = 'file:///home/s13g/Projects/devsearch/frontend/login.html'
+})
+
+
 let projectsUrl = 'http://127.0.0.1:8000/api/projects/'
 
 let getProjects = () => {
@@ -53,7 +71,7 @@ let addVoteEvents = () => {
                     'Content-Type': 'application/json',
                     Authorization: `Bearer ${token}`
                 },
-                body:JSON.stringify({'value': vote})
+                body: JSON.stringify({'value': vote})
             }).then(response => response.json()).then(data => {
                 getProjects()
             })
